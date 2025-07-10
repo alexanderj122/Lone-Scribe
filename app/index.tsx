5d634d3511be801df6b209460754f360600c37e4
Alexander Journeaux <84137663+alexanderj122@users.noreply.github.com>
2025-07-11 00:45:46 +0100

    Setup expo router to use app folder instead of App.tsx

---
+++ app/index.tsx
@@ -0,0 +1,9 @@
+import { Text } from 'react-native';
+
+const HomeScreen = () => {
+  return (
+    <Text className='text-center'>This is the home screen!</Text>
+  );
+};
+
+export default HomeScreen;
