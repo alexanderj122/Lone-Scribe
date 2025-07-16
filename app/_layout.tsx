22e8762d08cf47e3ce3da44137af49eb6fef1e47
Alexander Journeaux <84137663+alexanderj122@users.noreply.github.com>
2025-07-16 12:26:52 +0100

    Changed Layout to tabs, added each screen to the tablist with an icon.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,55 @@
-import { Stack } from 'expo-router';
-import "../global.css";
+import { Tabs } from 'expo-router';
+import { Ionicons } from '@expo/vector-icons';
+import '../global.css';
 
 export default function RootLayout() {
-  return <Stack></Stack>;
+  return (
+    <Tabs screenOptions={{ headerShown: false }}>
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: 'Home',
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="home-outline" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="play/index"
+        options={{
+          title: 'Play',
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="play-circle-outline" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="create/index"
+        options={{
+          title: 'Create',
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="add-circle-outline" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="settings/index"
+        options={{
+          title: 'Settings',
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="settings-outline" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="help/index"
+        options={{
+          title: 'Help',
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="help-circle-outline" color={color} size={size} />
+          ),
+        }}
+      />
+    </Tabs>
+  );
 }
